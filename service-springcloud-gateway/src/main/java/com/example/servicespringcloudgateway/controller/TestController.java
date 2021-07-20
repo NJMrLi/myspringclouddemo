2ecb6626cf4c07d05631c7753c08df8a6fd50972
licheng22 <licheng22@hikvision.com.cn>
2021-07-20 20:20:21 +0800

    [add] 增加一些gateway的predict配置

--- service-springcloud-gateway/src/main/java/com/example/servicespringcloudgateway/controller/TestController.java
+++ service-springcloud-gateway/src/main/java/com/example/servicespringcloudgateway/controller/TestController.java
@@ -4,11 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 @RestController
 public class TestController {
 
     @RequestMapping("/fallback")
     public Mono<String> fallback() {
         return Mono.justOrEmpty( "我是fallback方法" );
+    }
+
+
+    @RequestMapping("/test")
+    public Mono<String> test() {
+
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();
+        //输出时区时间
+        System.out.println(zonedDateTime);
+        return Mono.justOrEmpty( "Time: " + new Date());
     }
 }
